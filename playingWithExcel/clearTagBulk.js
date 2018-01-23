6adca9ac8515c7f708fa0b2a4c6f9a1799f4f309
Jenny <hzn2017@outlook.com>
2018-01-23 20:06:49 +0800

    Create clearTagBulk.js

---
+++ playingWithExcel/clearTagBulk.js
@@ -0,0 +1,83 @@
+var path = require('path'); 
+var fs = require("fs");
+var xlsx = require('node-xlsx');
+var rootPath = __filename ;  
+var current_dir = path.dirname(rootPath);
+var lastParentDir="";
+
+traverseDir(current_dir);
+
+function toClear(thePath,theName){
+    //读取文件内容
+    var obj = xlsx.parse(thePath+"\\"+theName);
+    var excelObj=obj[0].data;//只获取第一个sheet的内容
+    var data = [];
+    for(var i in excelObj){
+        var arr=[];
+        var value=excelObj[i];
+        for(var j in value){
+            //单元格内的值必须转成字符串之后才能用replace
+            var newVal = String(value[j]);
+            //保留图片、段落、空行、表格、有序和无序列表，其他标签全部清除
+            var noTagTxt = newVal.replace(/<\/?(?!img|\/?p|br|\/?table|\/?tbody|\/?tr|\/?th|\/?td|\/?ul|\/?ol|\/?li)[^>]+>/gm,"");
+            //清除所有的style，如果不是用style写的则会保留
+            var noStyleTxt = noTagTxt.replace(/\sstyle=\".+\"/gm,"");
+            arr.push(noStyleTxt);
+        }
+        data.push(arr);
+    }
+    var buffer = xlsx.build([
+        {
+            name:'sheet1',
+            data:data
+        }        
+    ]);
+    var regExp = /\.xls(x)?/;
+    var newExtent = theName.replace(regExp,".xlsx");
+    var newFileName = "cleared-" + newExtent;
+    //将文件内容插入新的文件中
+    fs.writeFileSync(thePath+"\\"+newFileName,buffer,{'flag':'w'});
+    console.log(thePath+"\\"+newFileName + " created");
+}
+
+function clearTag(filepath){
+    fs.stat(filepath,function(err,stats){
+        if (err) {
+            console.log(err);
+            return;
+        }
+        if(stats.isFile()){
+            var filename = path.basename(filepath);//filename 
+            var parentDir = path.dirname(filepath);//directory
+
+            var parentDirname = path.basename(path.dirname(filepath));//foldername
+            var thisFilename = path.basename(__filename);//this file name : nameAfterParent2.js
+            var fileFormat = filename.split(".");
+            if(fileFormat[fileFormat.length - 1] === "xls" || fileFormat[fileFormat.length - 1] === "xlsx"){
+                toClear(parentDir,filename);
+            }
+            lastParentDir = parentDir;
+        }else if(stats.isDirectory()){
+            console.log("======["+filepath+"] isDir=====");
+            traverseDir(filepath);
+        }else{
+        console.log("unknown type of file");
+        }
+    });
+}
+
+function traverseDir(dir){
+    fs.readdir(dir, function(error,files){
+        if (error){
+          console.log(error.stack);
+          return;
+        }
+        var len = files.length;
+        var file;
+        for(var i=0; i<len ;i++ ){
+            file = files[i]; 
+            thePath = dir;
+            clearTag(dir+"\\"+file);
+        }
+    });
+}

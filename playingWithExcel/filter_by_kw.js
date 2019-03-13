fc3bf111c33ecf5b03983ee3f412e2f95da2103a
Jenny <hzn2017@outlook.com>
2019-03-13 09:47:59 +0800

    Update filter_by_kw.js

--- playingWithExcel/filter_by_kw.js
+++ playingWithExcel/filter_by_kw.js
@@ -1,45 +1,51 @@
-var path = require('path'); 
+var path = require('path');
 var fs = require("fs");
-var CSV = require('csv-string');
 const csv2json = require('csvjson-csv2json');
 const json2csv = require('csvjson-json2csv');
-var rootPath = __filename ;  
+var rootPath = __filename;
 var current_dir = path.dirname(rootPath);
 
-processFile(current_dir,"keyword.csv", "data.csv");
-function processFile(thePath,kw_csv, data_csv){
-	fs.readFile(thePath+"\\"+kw_csv, function (err, data) {
-	    if (err) {
-	        console.log(err.stack);
-	        return;
-	    }
-	    var kw_arr = CSV.parse(String(data));//two dimension array
-	    
-	    kw_arr.shift();
-	    for (let j = 0; j < kw_arr.length; j++) {
-	    	kw_arr[j] = kw_arr[j][0].toLowerCase();
-	    }
-	    processData(kw_arr);
+processFile(current_dir, "keyword.csv", "data.csv");
+
+function processFile(thePath, kw_csv, data_csv) {
+	fs.readFile(thePath + "\\" + kw_csv, function (err, data) {
+		if (err) {
+			console.log(err.stack);
+			return;
+		}
+		var kw_arr = [];
+		var kw_json = csv2json(String(data));//json array [{a: 1}, {a: 2}, {a: 3}, {a: 4}]
+		var kw_header_arr = [];
+		for (let key in kw_json[0]) {
+			kw_header_arr.push(key);
+		}
+		for (let j = 0; j < kw_json.length; j++) {
+			kw_arr.push(kw_json[j][kw_header_arr[0]].toLowerCase());
+		}
+		processData(kw_arr);
 	});
 
 	function processData(kw) {
-		fs.readFile(thePath+"\\"+data_csv, function (err, data) {
+		fs.readFile(thePath + "\\" + data_csv, function (err, data) {
 			var output_arr = [];
-		    if (err) {
-		        console.log(err.stack);
-		        return;
-		    }
-		    var rawData = csv2json(String(data), {parseNumbers: true});//json array [{a: 1, b: 2}, {a: 3, b: 4}]
-		    var dynamicHeader = [];
-		    for (let i = 0; i < rawData.length; i++) {
-		    	let text = " ";
-	    		for (let k in rawData[i]) {
-	    			text += String(rawData[i][k]).toLowerCase() + " ";
-	    		}
-	    		let pureText = text.replace(/(\[|\]|\{|\}|\-|\(|\)|\"|:|,|\/)/g, " ");
+			if (err) {
+				console.log(err.stack);
+				return;
+			}
+			//json array [{a: 1, b: 2}, {a: 3, b: 4}]
+			var rawData = csv2json(String(data), {
+				parseNumbers: true
+			});
+			var dynamicHeader = [];
+			for (let i = 0; i < rawData.length; i++) {
+				let text = " ";
+				for (let k in rawData[i]) {
+					text += String(rawData[i][k]).toLowerCase() + " ";
+				}
+				let pureText = text.replace(/(\[|\]|\{|\}|\-|\(|\)|\"|:|,|\/)/g, " ");
 
 				let pureTextSimplified = pureText.replace(/\s{2,}/, " ");
-				
+
 				let kw_matched = [];
 				for (let j = 0; j < kw.length; j++) {
 					if (pureTextSimplified.indexOf(" " + kw[j] + " ") != -1) {
@@ -51,14 +57,12 @@ function processFile(thePath,kw_csv, data_csv){
 					rawData[i]["matched"] = kw_matched_str;
 					output_arr.push(rawData[i]);
 				}
-			    console.log("line " + i + " is done");
-		    }
-		    var output = json2csv(output_arr);
-		    fs.writeFileSync(__dirname + "\\" + "report.csv",output,{'flag':'w'});
+				console.log("line " + i + " is done");
+			}
+			var output = json2csv(output_arr);
+			fs.writeFileSync(__dirname + "\\" + "report.csv", output, {
+				'flag': 'w'
+			});
 		});
 	}
 }
-
-
-
-
